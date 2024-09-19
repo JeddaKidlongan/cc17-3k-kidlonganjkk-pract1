a9101ede9c19b35574b2450b1f534291ad6901db
JeddaKidlongan <kingadjedda@gmail.com>
2024-09-19 19:06:40 +0800

    Business card

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.practiceactivity1_businesscardapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.practiceactivity1_businesscardapp"
